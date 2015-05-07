a75febbb93d4c18545a043b786cf17f470eea101
mwcarlis <matthew.carlis@sjsu.edu>
2015-05-07 10:41:19 -0700

    Incomplete perceptron implementation.
    Using numpy to simplify operations and stuck currently
    on the logistics function

--- xo_learner.py
+++ xo_learner.py
@@ -1,9 +1,55 @@
 """ Perceptron Algorithm.
 -Matthew Carlis
 """
-
 import sys
+from numpy import array, full
 
+class network(object):
+
+    def __init__(self, matrix, trainset, epsilon=0.5, initial_w=0.5):
+        self.epsilon = epsilon
+        # Numpy Array.  A + B rows/col wise.
+        #               A * B Row/Column wise.  Not Matrix Mult.
+        #               5 + A Row/Column Wise.
+        # np.array([1, 2, 3]) * np.array([4, 5, 6]) = np.array([4, 10, 18])
+        self.matrix = array(matrix)
+        self.trainset = array(trainset)
+        self.weights = full((3, 3), 0.5)
+        self.output_y = full((3,3), 0)
+        print self.matrix.__repr__()
+        for data in self.trainset:
+            print data.__repr__()
+
+    def _matrix_logistic(self):
+        """ Return 1 / (1 + e ^ (- (W dot X)))
+        """
+        denom = 1.0 + np.exp(-(self.weights * self.matrix))
+        return 1.0 / denom
+
+    def _matrix_loss(self):
+        """ for all x, y in Set. Sum((y - logistic(x))^2).
+        """
+        return np.sum((self.output_y - self._matrix_logistic())**2)
+
+    def _alpha_t(self, time):
+        return 1000.0 / (1000.0 + time)
+
+    def _matrix_new_weights(self, time):
+        """ new W_i function.  Matrix operation.
+        """
+        x_i, w_i, y_i = self.matrix, self.weights, self.output_y
+        h_xi = self._matrix_logistic()
+        product = self.alpha_t(time) * (y_i - h_xi) * h_xi * (1.0 - h_xi) * x_i
+        return w_i + product
+
+    def run_train(self):
+        """ Run the training algorithm.
+        """
+        for time in xrange(1000):
+            weights = self._matrix_new_weights(time)
+            if self.matrix_loss() <= self.epsilon:
+                break
+        print 'Done Running'
 
 
 def get_matrix(file_name, matrix):
@@ -14,7 +60,8 @@ def get_matrix(file_name, matrix):
     with open(file_name, 'r') as file_d:
         for cnt, line in enumerate(file_d):
             row = []
-            data = line.rstrip('\n')
+            data = line.strip('\n')
+            data = data.strip('\r')
             if len(data) == 0:
                 matrix.append(t_matrix)
                 t_matrix = []
@@ -27,12 +74,11 @@ def get_matrix(file_name, matrix):
 
 def parse_input(args):
     if len(args) != 3:
-        sys.exit(1)
-
-    train_file = args[1]
-    test_file  = args[2]
-    train_cases = []
-    test_matrix = []
+        print 'Failed Arguments'
+        return
+        #sys.exit(1)
+    train_file, test_file = args[1], args[2]
+    train_cases, test_matrix = [], []
     get_matrix(train_file, train_cases)
     get_matrix(test_file, test_matrix)
     return train_cases, test_matrix[0]
@@ -52,11 +98,32 @@ def build_vector_map(dataset, d_map):
                 break
     return d_map
 
+def remap_values(train_set, test_case, d_map):
+    trainer = []
+    for data_set in train_set:
+        mat = []
+        for d_row in data_set:
+            row = []
+            for item in d_row:
+                row.append(d_map[item])
+            mat.append(row)
+        trainer.append(mat)
+    tester = []
+    for data_set in test_case:
+        row = []
+        for item in data_set:
+            row.append(d_map[item])
+        tester.append(row)
+    return trainer, tester
+
+
 
 if __name__ == "__main__":
     ARGS = sys.argv
+    # Map and Parse the input.
     TRAIN_SET, TEST_CASE = parse_input(ARGS)
     D_MAP = build_vector_map(TRAIN_SET, {})
-    print D_MAP
-    print TRAIN_SET
-    print TEST_CASE
+    TRAIN_VALS, TEST_VALS = remap_values(TRAIN_SET, TEST_CASE, D_MAP)
+    
+    # Start the Training/Learning
+    NET = network(TEST_VALS, TRAIN_VALS)
